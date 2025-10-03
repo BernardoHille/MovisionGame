fcee7bff6410cd41afe55070aed4d11a72b7b16b
Bernardo <bernardo@aeonvr.com.br>
2025-10-03 14:20:57 -0300

    Restore

--- sketch.js
+++ sketch.js
@@ -3,31 +3,56 @@
  * Learn more about the ml5.js project: https://ml5js.org/
  * ml5.js license and Code of Conduct: https://github.com/ml5js/ml5-next-gen/blob/main/LICENSE.md
  *
- * This example demonstrates pose tracking on live video through ml5.bodyPose with MoveNet model.
+ * This example demonstrates drawing skeletons on poses for the MoveNet model.
  */
 
 let video;
 let bodyPose;
 let poses = [];
+let connections;
 
-async function setup() {
-  createCanvas(640, 480);
-
-  // Load the bodyPose model asynchronously
-  bodyPose = await ml5.bodyPose();
-
-  // Create the video and hide it
-  video = createCapture(VIDEO);
-  video.size(640, 480);
+function preload() {
+  // Load the bodyPose model
+  bodyPose = ml5.bodyPose();
+  
+   // Create the video and hide it
+  video = createCapture(VIDEO, false);
   video.hide();
+}
+
+function setup() {
+  // q5.js gets HD resolution from the webcam
+  // by default, so make the canvas match the
+  // capture size
+  createCanvas(video.width, video.height);
+  displayMode(MAXED);
 
   // Start detecting poses in the webcam video
   bodyPose.detectStart(video, gotPoses);
+  // Get the skeleton connection information
+  connections = bodyPose.getSkeleton();
 }
 
 function draw() {
   // Draw the webcam video
   image(video, 0, 0, width, height);
+
+  // Draw the skeleton connections
+  for (let i = 0; i < poses.length; i++) {
+    let pose = poses[i];
+    for (let j = 0; j < connections.length; j++) {
+      let pointAIndex = connections[j][0];
+      let pointBIndex = connections[j][1];
+      let pointA = pose.keypoints[pointAIndex];
+      let pointB = pose.keypoints[pointBIndex];
+      // Only draw a line if both points are confident enough
+      if (pointA.confidence > 0.1 && pointB.confidence > 0.1) {
+        stroke(255, 0, 0);
+        strokeWeight(2);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+      }
+    }
+  }
 
   // Draw all the tracked landmark points
   for (let i = 0; i < poses.length; i++) {
